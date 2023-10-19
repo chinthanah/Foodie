5d41ca97a1746e7546a70692a7e434e0fcc8d817
chinthanah <chinthanahssuresh@gmail.com>
2023-10-19 20:46:02 +0530

    added carousel

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -2,14 +2,22 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
     <div>
       <div>
         <Navbar />
+      </div>
+      <div>
+        <Carousel />
+      </div>
+      <div>
         <Card />
       </div>
+      <Card />
+
       <div>
         <Footer />
       </div>
